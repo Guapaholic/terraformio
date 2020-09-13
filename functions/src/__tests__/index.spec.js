fd9e8b66067842b81dbde584df8b433134a0ae84
Pedro De Oña <pdeona@rbi.com>
2020-09-13 03:50:20 -0400

    fix ci

--- functions/src/__tests__/index.spec.js
+++ functions/src/__tests__/index.spec.js
@@ -1,13 +1,15 @@
 const { handler } = require('..')
 
 describe('handler', () => {
-  it('returns statusCode, name, and boatId', () => {
-    const result = await handler({ foo: 'bar' }, { fiz: 'baz' });
+  it('returns statusCode, name, and boatId', async () => {
+    const result = await handler({ foo: 'bar' }, { fiz: 'baz' })
 
-    expect(result.statusCode).toEqual(200);
-    expect(JSON.parse(result.body)).toEqual(expect.objectContaining({
-      name: expect.any(String),
-      boatId: expect.any(String),
-    }))
+    expect(result.statusCode).toEqual(200)
+    expect(JSON.parse(result.body)).toEqual(
+      expect.objectContaining({
+        name: expect.any(String),
+        boatId: expect.any(String),
+      }),
+    )
   })
 })
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/functions/src'],
-  testMatch: ['<rootDir>/**/__tests__/**/*.spec.ts'],
+  testMatch: ['<rootDir>/**/__tests__/**/*.(spec|test).js'],
   testPathIgnorePatterns: ['dist'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'node',
